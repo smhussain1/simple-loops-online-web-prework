6e066271dd28278d8016a0c764a8d503d960e6c3
michael hussain <smhussain_1@yahoo.com>
2018-12-17 21:56:38 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,7 @@ def loop_iterator(number_of_times)
     loop do
       number_of_times = number_of_times+1 
       puts "Welcome to Flatiron School's Web Development Course!"
-      if counter >= 7
+      if number_of_times >= 7
         break
       end
     end
