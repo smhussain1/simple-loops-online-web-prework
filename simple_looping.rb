4b987648d83f61d14977b84ef241a72c3bd215b6
michael hussain <smhussain_1@yahoo.com>
2018-12-17 21:54:22 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -9,7 +9,7 @@ def loop_iterator(number_of_times)
       if counter >= 7
         break
       end
-  
+  # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
   # How can we make sure the loop breaks once it has puts out the phrase the 
