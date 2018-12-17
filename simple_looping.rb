296e2c12740c90da6d9638e23f4ee27cbe445c37
michael hussain <smhussain_1@yahoo.com>
2018-12-17 21:54:51 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,6 @@
 def loop_iterator(number_of_times)
   
     loop do
-      # phrase = "Welcome to Flatiron School's Web Development Course!"
       counter = counter+1 
       puts "Welcome to Flatiron School's Web Development Course!"
       if counter >= 7
