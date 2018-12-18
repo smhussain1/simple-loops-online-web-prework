1f9d3180e632fd19cd603c2f7666a3a411149ca2
michael hussain <smhussain_1@yahoo.com>
2018-12-18 01:08:27 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -40,7 +40,7 @@ def while_iterator(number_of_times)
 end
 
 def until_iterator(number_of_times)
-   until number_of_times = 14
+   until number_of_times == 14
   puts "Welcome to Flatiron School's Web Development Course!"
   number_of_times += 1 
   end
