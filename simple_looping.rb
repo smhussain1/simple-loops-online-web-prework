87d726aac403ee7d15263a61f20f1aa3bdd58cd2
michael hussain <smhussain_1@yahoo.com>
2018-12-18 00:35:49 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,7 +21,7 @@ end
 
 
 def times_iterator(number_of_times)
-  10.times do
+  7.times do
   puts "Welcome to Flatiron School's Web Development Course!"
   end
   # phrase = "Welcome to Flatiron School's Web Development Course!"
